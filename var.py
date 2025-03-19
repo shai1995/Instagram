801948864e5fcdf3d28f65b2e1cce7c4077b687e
Mradbot_Admin <76399884+shai1995@users.noreply.github.com>
2025-03-19 22:00:16 +0530

    Update var.py

--- var.py
+++ var.py
@@ -15,7 +15,7 @@ DB_URI = os.getenv("DB_URI", "mongodb+srv://xxxx:xxxx@xxxx")
 DB_NAME = os.getenv("DB_NAME", "instalaoder")
 
 IS_FSUB = bool(os.environ.get("FSUB", True)) # Set "True" For Enable Force Subscribe
-AUTH_CHANNELS = os.environ.get("AUTH_CHANNEL", "-100xxxxxxx") # Add Multiple Channels iD By Space
+AUTH_CHANNELS = os.environ.get("AUTH_CHANNEL", "-1001315120330") # Add Multiple Channels iD By Space
 AUTH_CHANNELS = [int(channel_id) for channel_id in AUTH_CHANNELS.split(",")] # DONT TOUCH
 
 
